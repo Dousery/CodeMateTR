32e129b2d9c705b0311fd5a5b03db837dafa2508
Dousery <doguser15@gmail.com>
2025-08-06 01:10:24 +0300

    try

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,8 +10,10 @@ export default defineConfig({
         manualChunks: undefined,
       },
     },
+    outDir: 'dist',
   },
   server: {
     historyApiFallback: true,
   },
+  publicDir: 'public',
 })
